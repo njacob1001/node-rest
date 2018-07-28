ec8cbc8308a75c07e449525f38ed93df23151775
njacob1001 <njacob1001@gmail.com>
2018-07-28 15:25:26 -0500

    REST server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('./config/config');
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 //settings
 
@@ -9,41 +10,14 @@ const app = express();
 //Middlewares
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
+app.use( require('./routes/usuario'));
 
-app.get('/usuario', function (req, res) {
-    res.json('get Usuario');
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true } ,(err,resp)=>{
+    if(err) throw err;
+
+    console.log('Base de datos ONLINE');
+    
 });
-
-app.post('/usuario', function (req, res) {
-    let body = req.body;
-
-    if(body.nombre === undefined){
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    }else {
-        res.json({
-            persona: body
-        });
-    }
-
-    res.json({
-        persona: body
-    });
-});
-
-app.put('/usuario/:id', function (req, res) {
-    let id = req.params.id;
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', function (req, res) {
-    res.json('delete Usuario')
-});
-
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto', process.env.PORT);
